8cf5479f6faa2a6953b52b36f4d185cfedc16118
Aadi-Y <procoderaadi33@gmail.com>
2025-09-18 14:37:09 +0530

    Changed

--- Frontend/src/Utility/apiPath.js
+++ Frontend/src/Utility/apiPath.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "https://ai-interview-preparation-vkrz.vercel.app";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://ai-interview-preparation-ejm2.onrender.com/";
 
 export const API_PATHS = {
     AUTH:{
