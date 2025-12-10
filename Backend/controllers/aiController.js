d317c0f7bf3478d8e45c519dfeb047fa99fed36b
Aadi-Y <procoderaadi33@gmail.com>
2025-12-10 10:42:16 +0530

    Changed

--- Backend/controllers/aiController.js
+++ Backend/controllers/aiController.js
@@ -1,57 +1,64 @@
-const {GoogleGenAI} = require("@google/genai");
-const {questionAnswerPrompt,
-    conceptExplainPrompt} = require("../utils/prompt");
+const { GoogleGenAI } = require("@google/genai");
+const { questionAnswerPrompt,
+    conceptExplainPrompt } = require("../utils/prompt");
 
-const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 
 // @desc Generate interview questions and answers using Gemini
 // @route POST /api/ai/generate-questions
 // @access Private
-const generateQuestions = async(req,res) => {
-    try{
-        const {role,
+const generateQuestions = async (req, res) => {
+    try {
+        const { role,
             experience,
             topicToFocus,
             numberOfQuestions
         } = req.body
 
-        if(!role || !experience || !topicToFocus || !numberOfQuestions) {
+        if (!role || !experience || !topicToFocus || !numberOfQuestions) {
             return res.status(400).json(
                 {
-                    error:true,
-                    message:"All field required"
+                    error: true,
+                    message: "All field required"
                 }
             )
         }
 
-        const prompt = questionAnswerPrompt(role,experience,topicToFocus,numberOfQuestions);
+        const prompt = questionAnswerPrompt(role, experience, topicToFocus, numberOfQuestions);
         console.log(prompt);
 
         const response = await ai.models.generateContent({
-            model:"gemma-3-4b",
-            contents:prompt
-        })
-        
+            model: "gemma-3-4b",
+            contents: [
+                {
+                    parts: [
+                        { text: prompt }
+                    ]
+                }
+            ]
+        });
+
+
         const rawText = response.text;
 
 
         //Clean the response before use
         const cleanedText = rawText
-        .replace(/^```json\s*/, "") // remove starting of json
-        .replace(/```$/, "") //remove ending of json
-        .trim(); //remove extra spaces
+            .replace(/^```json\s*/, "") // remove starting of json
+            .replace(/```$/, "") //remove ending of json
+            .trim(); //remove extra spaces
 
         const data = JSON.parse(cleanedText);
-        
+
         res.status(200).json({
-            error:false,
+            error: false,
             data
         })
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            error:true,
-            message:error.message
+            error: true,
+            message: error.message
         })
     }
 }
@@ -59,41 +66,41 @@ const generateQuestions = async(req,res) => {
 // @desc Generate concept explanation using Gemini
 // @route POST /api/ai/generate-explanation
 // @access Private
-const generateExplanation = async(req,res) => {
-    try{
-        const {question} = req.body;
+const generateExplanation = async (req, res) => {
+    try {
+        const { question } = req.body;
 
-        if(!question){
+        if (!question) {
             return res.status(404).json({
-                error:true,
-                message:"Question is missing please pass question"
+                error: true,
+                message: "Question is missing please pass question"
             })
         }
 
         const prompt = conceptExplainPrompt(question);
 
         const response = await ai.models.generateContent({
-            model:"gemma-3-4b",
-            contents:prompt
+            model: "gemma-3-4b",
+            contents: prompt
         })
 
         let rawText = response.text;
 
         const cleanedText = rawText
-        .replace(/^```json\s*/, "") // remove starting of json
-        .replace(/```$/, "") //remove ending of json
-        .trim(); //remove extra spaces
+            .replace(/^```json\s*/, "") // remove starting of json
+            .replace(/```$/, "") //remove ending of json
+            .trim(); //remove extra spaces
 
         const data = JSON.parse(cleanedText);
 
         res.status(201).json({
-            error:false,
+            error: false,
             data
         })
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            error:true,
-            message:error.message
+            error: true,
+            message: error.message
         })
     }
 }
